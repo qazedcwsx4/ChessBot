900afbd6e969ce5ee9c800e2c25fa5e154caf68c
qaze <qazedcwsx4@gmail.com>
2021-01-23 14:50:07 +0100

    Refactored and cleaned up code

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -1,52 +1,37 @@
 use hyper::{body::HttpBody as _, Client, Request, Body};
-use hyper::header;
-use tokio::io;
-use serde_json::{Value};
-use hyper::http::HeaderValue;
 use hyper_tls::HttpsConnector;
-use std::thread::{sleep};
-use std::time;
-use std::error::Error;
-use tokio::task;
 use tokio::task::JoinHandle;
 use std::sync::mpsc::{self, Sender, Receiver};
-use tokio::runtime::Runtime;
-use tokio::time::Duration;
 use crate::stream::platform_event::Event;
 
 mod platform_event;
 
 const URL: &str = "https://lichess.org/api";
-const ENDPOINT: &str = "stream/event";
+const EVENT_ENDPOINT: &str = "stream/event";
 const BEARER_TOKEN: &str = "eOcwgviIZY7b9ZU3";
 const EMPTY_MESSAGE: &str = "\n";
 
-pub fn start_event_thread_blocking() -> (JoinHandle<()>, Receiver<String>) {
-    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
-    let rt = Runtime::new().unwrap();
+pub async fn get_incoming_event_stream() -> (JoinHandle<()>, Receiver<String>){
+    get_event_stream(EVENT_ENDPOINT).await
+}
 
-    let handle = rt.spawn(start_events(tx));
+async fn get_event_stream(endpoint: &'static str) -> (JoinHandle<()>, Receiver<String>) {
+    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+
+    let handle = tokio::spawn(start_event_thread(tx, endpoint));
 
     (handle, rx)
 }
 
-pub async fn start_event_thread() -> (JoinHandle<()>, Receiver<String>) {
-    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
-
-    let handle = tokio::spawn(start_events(tx));
-
-    (handle, rx)
+async fn start_event_thread(tx: Sender<String>, endpoint: &str) {
+    if let Err(e) = events(tx, endpoint).await { panic!(e.to_string()) }
 }
 
-async fn start_events(tx: Sender<String>) {
-    if let Err(e) = events(tx).await { panic!(e.to_string()) }
-}
-
-async fn events(tx: Sender<String>) -> std::result::Result<(), Box<dyn std::error::Error>> {
+async fn events(tx: Sender<String>, endpoint: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
     println!("Starting the listener");
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let request = Request::get(format!("{}/{}", URL, ENDPOINT))
+    let request = Request::get(format!("{}/{}", URL, endpoint))
         .header("Authorization", format!("Bearer {}", BEARER_TOKEN))
         .body(Body::from(""))?;
 
@@ -57,7 +42,7 @@ async fn events(tx: Sender<String>) -> std::result::Result<(), Box<dyn std::erro
         let slice = std::str::from_utf8(chunk.as_ref())?;
 
         if slice != EMPTY_MESSAGE {
-            tx.send(slice.to_string());
+            tx.send(slice.to_string())?;
 
             let xd: Event = serde_json::from_str(slice).unwrap();
 
